b0edae6d1bc52c71f5c68e43c34922a373f48996
todoroki <s18.todoroki+git@gmail.com>
2015-06-16 18:09:13 +0900

    [update] dbcreate.rb : ベタ書きをやめて関数化
    今まで処理を全てベタ書きしていたが、dbcreateという関数とした
    その返り値を取得したIDとする

--- dbcreate.rb
+++ dbcreate.rb
@@ -3,41 +3,47 @@
 require 'rubygems'
 require 'sqlite3'
 
-unless File.exists?("config") then
-  `mkdir config`
-end
+def dbcreate()
+  unless File.exists?("config") then
+    `mkdir config`
+  end
 
-unless File.exists?("config/location.db") then
-  db = SQLite3::Database.new("config/location.db")
-  sql = <<SQL
+  unless File.exists?("config/location.db") then
+    db = SQLite3::Database.new("config/location.db")
+    sql = <<SQL
 create table Location (
 name varchar(10),
 num  integer
 );
 SQL
-  db.execute(sql)
+    db.execute(sql)
 
-  db.transaction do
-    sql = "insert into Location values (?, ?)"
-    File.open("data.txt").each_line do |line|
-      line = line.split(",")
-      db.execute(sql, line[0], line[1].chomp.to_i)
+    db.transaction do
+      sql = "insert into Location values (?, ?)"
+      File.open("data.txt").each_line do |line|
+        line = line.split(",")
+        db.execute(sql, line[0], line[1].chomp.to_i)
+      end
     end
+    db.close
+  end
+
+  db = SQLite3::Database.open("config/location.db")
+  name = "仙台"
+  id = 0
+  db.execute("select * from Location where name=\"#{name}\"") do |row|
+    id = row[1]
   end
   db.close
+
+  if id == 0 then
+    puts "Sorry, #{name} does not exist in DATABASE."
+  else
+    puts "id: #{id}"
+  end
+  id
 end
 
-db = SQLite3::Database.open("config/location.db")
-name = "a"
-id = 0
-db.execute("select * from Location where name=\"#{name}\"") do |row|
-  id = row[1]
+if __FILE__ == $0 then
+  dbcreate()
 end
-db.close
-
-if id == 0 then
-  puts "Sorry, #{name} does not exist in DATABASE."
-else
-  puts "id: #{id}"
-end
-
